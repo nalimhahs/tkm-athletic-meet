7aed8c471c5fd4324f03b751768332646e3a9e3f
Milan Shah <milanshah1@gmail.com>
2020-01-23 00:13:25 +0530

    Added logging

--- sports/settings.py
+++ sports/settings.py
@@ -130,3 +130,36 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 import django_heroku
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'sports.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'sports': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
